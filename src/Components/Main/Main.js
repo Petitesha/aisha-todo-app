e07dba7ac6e79298586464a43b78d2e800b62651
Bello aisha <belloaishaaderonke@gmail.com>
2022-09-21 12:33:14 +0100

    first-commit

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,16 @@
+import classes from '../Main/Main.module.css';
+
+
+
+function Main() {
+    return (
+      <div className={classes.main}>
+       <div className={classes.innerMain}>
+        <p>This is the main page</p>
+       </div>
+      </div>
+    );
+  }
+  
+  export default Main;
+  
